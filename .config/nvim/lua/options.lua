f3a550d49facfc3a32508a8a116d5668aa1df928
Jon Mooring <jonmooring@gmail.com>
2024-06-21 15:33:28 -0700

    Update formatting for Neovim config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,29 +7,29 @@ vim.opt.directory = HOME .. '/.config/nvim/swap'
 vim.opt.undodir = HOME .. '/.config/nvim/undo'
 
 -- Enable basic indentation, smarter than "smart"indent
-vim.opt.autoindent  = true
+vim.opt.autoindent = true
 -- Highlight the current line
-vim.opt.cursorline  = true
+vim.opt.cursorline = true
 -- Start searching as we start typing
-vim.opt.incsearch   = true
+vim.opt.incsearch = true
 -- Highlight what we're searching for
-vim.opt.hlsearch    = true
+vim.opt.hlsearch = true
 -- Always show the currently entered command
-vim.opt.showcmd     = true
+vim.opt.showcmd = true
 -- Make a backup before overwriting a file
 vim.opt.writebackup = true
 -- Make vim more responsive
-vim.opt.ttyfast     = true
+vim.opt.ttyfast = true
 -- Don't show intermediate macro steps
-vim.opt.lazyredraw  = true
+vim.opt.lazyredraw = true
 -- Auto wrap comments at 80 chars
-vim.opt.textwidth   = 80
+vim.opt.textwidth = 80
 -- Allow buffer switching without saving
-vim.opt.hidden      = true
+vim.opt.hidden = true
 -- No point since we use lualine
-vim.opt.showmode    = false
+vim.opt.showmode = false
 -- Ensure we have a buffer of 5 lines at the top and bottom
-vim.opt.scrolloff   = 5
+vim.opt.scrolloff = 5
 -- Enable mouse support
 vim.opt.mouse = 'a'
 
